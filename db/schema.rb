8557feb56db7790f99394000cab76c5c9c3c9572
koujinakajima <f.f-super1142@au.com>
2023-04-14 16:09:37 +0900

    null制約と空文字のdefaultd設定

--- db/schema.rb
+++ db/schema.rb
@@ -22,16 +22,16 @@ ActiveRecord::Schema.define(version: 2023_04_12_143434) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "password", limit: 64
-    t.string "last_name", limit: 16
-    t.string "first_name", limit: 16
-    t.string "zipcode", limit: 16
-    t.string "prefecture", limit: 16
-    t.string "municipality", limit: 16
-    t.string "address", limit: 32
-    t.string "apartments", limit: 32
-    t.string "email", limit: 128
-    t.string "phone_number", limit: 16
+    t.string "password", limit: 64, default: "", null: false
+    t.string "last_name", limit: 16, default: "", null: false
+    t.string "first_name", limit: 16, default: "", null: false
+    t.string "zipcode", limit: 16, default: "", null: false
+    t.string "prefecture", limit: 16, default: "", null: false
+    t.string "municipality", limit: 16, default: "", null: false
+    t.string "address", limit: 32, default: "", null: false
+    t.string "apartments", limit: 32, default: "", null: false
+    t.string "email", limit: 128, default: "", null: false
+    t.string "phone_number", limit: 16, default: "", null: false
     t.bigint "user_classification_id"
     t.string "company_name", limit: 128
     t.boolean "delete_flag"
