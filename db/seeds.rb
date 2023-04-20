5b4f9a7f056f7b5a475b704c5255a3261073a25d
koujinakajima <f.f-super1142@au.com>
2023-04-20 12:31:10 +0900

    ユーザーデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+UserClassification.create(user_classification_name: "ゲスト")
+
+2.times do |i|
+  User.create!(last_name: "hoge",
+              first_name: "geho",
+              password: "foobar#{i+1}",
+              zipcode: "1111111",
+              prefecture: "東京都",
+              municipality: "港区",
+              address: "赤坂1-1",
+              apartments: "hogeハイム1-1",
+              email: "hogehoge#{i+1}@example.com",
+              phone_number: "09012345678",
+              user_classification: UserClassification.first,
+              company_name: "株式会社あんず",
+              delete_flag: false)
+end
