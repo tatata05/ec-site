e27a04d5c4f7f2325dcc6fce7b63e3b475405310
nakajimakouji <f.f-super1142@au.com>
2023-04-27 18:23:50 +0900

    ユーザー修正画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :last_name, :first_name, :zipcode, :prefecture, :municipality,
+                                 :address, :apartments, :email, :phone_number, :company_name)
+  end
+
 end
