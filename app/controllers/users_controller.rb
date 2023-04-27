d4ae33f91c45fb95f3aaa21ea8ea228c5b77c908
nakajimakouji <f.f-super1142@au.com>
2023-04-27 22:54:32 +0900

    updateアクション削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-  end
-
-  def update
     @user = User.find_by(id: params[:id])
-    @user.assign_attributes(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render 'edit'
-    end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:password, :last_name, :first_name, :zipcode, :prefecture, :municipality,
                                  :address, :apartments, :email, :phone_number, :company_name)
