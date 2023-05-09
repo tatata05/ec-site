4a35eeadf9949d8741e247503d2ee48f57573358
taishi0202 <taishi020294@gmail.com>
2023-05-10 03:20:15 +0900

    rubocopによる修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find_by(id: params[:id])
   end
@@ -7,5 +6,4 @@ class UsersController < ApplicationController
   def edit
     @user = User.find_by(id: params[:id])
   end
-
 end
