ef3396affe687d7d4c6e52b75fb4a0feb4b05b2b
tatata05 <t.t.syukatsu.t.t@gmail.com>
2023-05-15 18:06:56 +0900

    商品検索画面の実装(検索機能を除く)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,8 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all.page(params[:page])
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
   end
