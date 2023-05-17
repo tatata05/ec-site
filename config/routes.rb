da4c3de98d79064adb1b466747b6b87936d3e70c
nakajimakouji <f.f-super1142@au.com>
2023-05-18 02:19:40 +0900

    ログイン画面表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
   resources :users
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
