a2353eeb5cedec3c0f24637ee9c0e89a62f9b218
nakajimakouji <f.f-super1142@au.com>
2023-05-24 16:48:20 +0900

    ログイン、ログアウト処理

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
