7ac2c7cb70d9a3a1ff855a76d936159cb1394a26
nakajimakouji <f.f-super1142@au.com>
2023-05-25 10:30:22 +0900

    認可処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  include SessionsHelper
   def show
     @user = User.find_by(id: params[:id])
   end
 
   def edit
-    @user = User.find_by(id: params[:id])
   end
 
   def update
-    @user = User.find_by(id: params[:id])
     @user.assign_attributes(user_params)
     if @user.save
       flash[:success] = "更新しました"
@@ -25,4 +26,19 @@ class UsersController < ApplicationController
     params.require(:user).permit(:last_name, :first_name, :zipcode, :prefecture, :municipality, :address, :apartments, :email, :phone_number)
   end
   
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    if current_user != @user
+      flash[:danger] = "他人の情報にアクセスすることはできません"
+      redirect_to root_path
+    end
+  end
+
 end
