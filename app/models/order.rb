85d11c393a1355a1605ecb156fd6c307d6e37f3a
AoiSuzumura <suzu_ao20000211@yahoo.co.jp>
2023-05-30 23:27:23 +0900

    注文詳細画面#23

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,15 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details, dependent: :destroy
+
+  def order_preparing? 
+    order_details.any? do |order_detail|
+      order_detail.shipment_status_id == 1
+    end
+  end
+  
+
+  def total_price
+    order_details.sum(&:product_price)
+  end
 end
